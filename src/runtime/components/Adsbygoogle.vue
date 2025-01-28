1c54ac37b7dea21c6c3f059039980715d48528ab
nexoscreator <112468782+nexoscreator@users.noreply.github.com>
2025-01-28 15:57:19 +0530

    refactor(props): remove unnecessary withDefaults()
    eplaced withDefaults() in Adsbygoogle.vue with destructured default values for props to align with Vue's best practices and suppress compiler warnings.

--- src/runtime/components/Adsbygoogle.vue
+++ src/runtime/components/Adsbygoogle.vue
@@ -5,12 +5,19 @@ import type { AdFormats } from '../../module'
 import { useAdsense } from '#imports'
 
 const {
-  adClient,
-  analyticsDomainName,
+  adClient = undefined,
+  adSlot = null,
+  adFormat,
+  adLayout = null,
+  adLayoutKey = null,
+  adStyle = { display: 'block' },
+  adFullWidthResponsive = false,
+  hideUnfilled = undefined,
+  pageUrl = null,
   analyticsUacct,
-  hideUnfilled,
+  analyticsDomainName,
   includeQuery,
-} = withDefaults(defineProps<{
+} = defineProps<{
   adClient?: string
   adSlot?: string | null
   adFormat?: AdFormats | string
@@ -23,17 +30,7 @@ const {
   analyticsUacct?: string
   analyticsDomainName?: string
   includeQuery?: boolean
-}>(),
-{
-  adFullWidthResponsive: false,
-  adLayout: null,
-  adLayoutKey: null,
-  pageUrl: null,
-  adSlot: null,
-  adStyle: () => ({ display: 'block' }),
-  adClient: undefined,
-  hideUnfilled: undefined,
-})
+}>()
 
 const {
   adClass,
